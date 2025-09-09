48c9930f23ad835e4e6302b129ee75a0e47cc483
Ladislav Račák <racak@sentisquare.com>
2025-09-09 14:34:37 +0200

    fix route, remove repl it script

--- client/src/pages/translation-manager.tsx
+++ client/src/pages/translation-manager.tsx
@@ -77,7 +77,7 @@ export default function TranslationManager() {
         
         <main className="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8">
           <UploadSection onUploadComplete={(data) => {
-            window.location.href = `/project/${data.project.id}`;
+            window.location.href = `/Translation-Manager/project/${data.project.id}`;
           }} />
         </main>
       </div>
